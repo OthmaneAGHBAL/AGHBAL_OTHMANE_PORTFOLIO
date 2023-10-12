7136923c44b28e9adae2a544bc6ffa42a7ac4a79
OthmaneAGHBAL <aghbal.othmane9@gmail.com>
2023-10-13 01:47:13 +0200

    responsive mobile citation

--- src/components/Citation.js
+++ src/components/Citation.js
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 
 export const Citation = () => {
   return (
-    <section className="citation d-flex px-5 align-content-center shadow text-center justify-content-center fs-3 text-light py-4 ">
+    <section className="citation d-flex px-5 align-content-center shadow text-center justify-content-center fs-4 text-light py-4 ">
                <div className=" border-1 border-light border px-2">
                     Think Outside The
                </div>
